62060f4d1d78713625ae4e5a4d53b5e8112b98cb
KUKIMOTO Nobuyuki <kukimoto@h23.52.236.10.32103.vlan.kuins.net>
2014-07-23 09:50:48 +0900

    commit 2
    2回め

---
+++ test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by KUKIMOTO Nobuyuki on 2014/07/23.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ test/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  test
+//
+//  Created by KUKIMOTO Nobuyuki on 2014/07/23.
+//
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
